4079d128ae770e4f64e5c9a2810dc45da052f57c
Zade <57639667+Zadeson@users.noreply.github.com>
2024-03-10 14:09:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='VeilMail',
     version='1.0.7',
-    py_modules=['veilmail'],
+    packages=find_packages(),
     install_requires=[
         'requests',
         'rich',
